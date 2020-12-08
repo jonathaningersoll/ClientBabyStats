3c87702fee211a605986425597f9ee17d244f9a2
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-12-08 09:34:42 -0500

    altered styles for login

--- src/Components/PreAuth/Authorize.jsx
+++ src/Components/PreAuth/Authorize.jsx
@@ -12,12 +12,12 @@ export default class Authorize extends React.Component{
           return(
                <Container className="auth-container">
                     <Row>
-                         <Col sm="12" md={{ size: 6, offset: 3 }} className="login-col">
+                         <Col sm="12" md={{ size: 4, offset: 4 }} className="login-col">
                               <Login updateToken={this.props.updateToken} setRole={this.props.setRole} />
                          </Col>
                     </Row>
                     <Row>
-                         <Col md="4" className="register">
+                         <Col sm="12" md={{ size: 4, offset: 4 }} className="register">
                               <Register updateToken={this.props.updateToken} />
                          </Col>
                     </Row>
